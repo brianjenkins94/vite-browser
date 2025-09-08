7d60e28e2734c1fc7530749d34ccb0b3e80aa3e3
Brian Jenkins <brianjenkins94@gmail.com>
2025-09-08 18:46:39 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+rm -rf vite/ vite-src/
+
+git clone --no-checkout --depth 1 --filter=tree:0 --sparse https://github.com/vitejs/vite.git vite-src
+
+cd vite-src/
+
+git sparse-checkout set packages/vite/
+
+git checkout
+
+cd ..
+
+cp -rf vite-src/packages/vite/ vite/
+
+rm -rf vite-src/
+
+cd vite
+
+cd "$CWD"
