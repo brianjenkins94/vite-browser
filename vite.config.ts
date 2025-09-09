129f920b77cdee9aa7f460d51b1721da79fb4cf2
Brian Jenkins <brianjenkins94@gmail.com>
2025-09-09 08:27:31 -0400

    Start

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,20 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-
+    "build": {
+        "rollupOptions": {
+            "input": {
+                "env": "vite/src/client/env.ts",
+                "client": "vite/src/client/client.ts",
+                "browser": "vite/src/node/index.ts",
+                //"node": "vite/src/node/index.ts",
+                "cli": "vite/src/node/cli.ts"
+            },
+            "external": [
+                "#module-sync-enabled",
+                "lightningcss",
+                "./fsevents.node"
+            ]
+        }
+    }
 });
